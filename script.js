79212768a120f1857404e159f4409d1a23129b53
GiovannaMorais <gigimorais9@hotmail.com>
2023-02-25 11:18:27 -0300

    Fix: removing errors and cleaning up the code

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
+const saveCartItems = require('./helpers/saveCartItems');
+const getSavedCartItems = require('./helpers/getSavedCartItems');
+
 const cartItems = document.querySelector('.cart__items');
 const total = document.querySelector('.total-price');
 const esvaziar = document.querySelector('.empty-cart');
-// const sectionCart = document.querySelector('.cart');
 const sectionItems = document.querySelector('.items');
-
-// na funçao somarTotal,primeiro declarei a subtotal iniciando com valor = 0
-// chamei a função getSavedCartItems (que retorna o valor do item em string que está no local storage)
-// usei o split para dividir a string ao chegar no $ ,retornando um array 
-// usei o shift para deletar o primeiro item (que era o nome, o id do item e iria até o price) 
-// pegando somente o numero
-// fiz um forEach para dar um loop em todos os preços do array feito no split
-// somando na variavel subtotal o preço do itemAtual que está no loop
-// utilizei o parseFloat para converte a string em formato de numero em numeros reais
 
 const somarTotal = () => { 
   let subtotal = 0;
@@ -49,11 +42,6 @@ somarTotal();
 }
 cartItems.addEventListener('click', cartItemClickListener);
 
-// peguei a função createProductItemElement coloquei um addEventListener no botao adicionar ao carinho
-// e chamei a funçao adicionarProduct  onde chamei o obj fetchItem
-// e appendiei o resultado da função createCartItemElement no carrinho de compras
-// e dps retornei apendiei o botao na section da funçao createProductItemElement
-
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -64,11 +52,8 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 }
 async function adicionarProduct(product) {
   const result = await fetchItem(product);
-  // console.log("result", result);
   cartItems.appendChild(createCartItemElement(result));
   const productCart = cartItems.innerHTML;
-  // console.log("productCart", productCart);
-  // console.log(productCart);
   saveCartItems(productCart);
   somarTotal();
 }
@@ -92,9 +77,9 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 const loading = () => {
   const load = document.createElement('span');
   load.classList = 'loading';
@@ -106,25 +91,15 @@ const unloading = () => {
   sectionItems.removeChild(unload);
 };
 
-// Peguei o obj pelo items da função fetchProducts
-// usei o forEach para percorrer todos os produtos
-// e appendei na section .items todos os produtos que foram
-// percorridos pelo forEach
-// chamei a funçao no window.onload com o item que estou procurando
-// que no caso seria o computador
-
 const listaProdutos = async (item) => {
   loading();
   const items = await fetchProducts(item);
-  // console.log(items);
-  // const sectionItems = document.querySelector('.items');
   items.results.forEach((produto) =>
     sectionItems.appendChild(createProductItemElement(produto)));
     unloading();
 };
 
 const esvaziarCarrinho = () => {
-//  cartItems.innerHTML = '';
  saveCartItems(cartItems.innerHTML = '');
  somarTotal();
 };
